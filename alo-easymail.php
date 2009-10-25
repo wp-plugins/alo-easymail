0b9f529ebfe9486931e5cb5d08e5bc6a2d38977e
eventualo <eventualo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-25 22:28:05 +0000

    update version
    git-svn-id: https://plugins.svn.wordpress.org/alo-easymail/tags/0.9.2@166948 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- alo-easymail.php
+++ alo-easymail.php
@@ -3,7 +3,7 @@
 Plugin Name: ALO Easy Mail
 Plugin URI: http://www.eventualo.net/blog/?p=365
 Description: Allows you to send e-mails and newsletters to your registered users and to other e-mail addresses.
-Version: 0.9.1
+Version: 0.9.2
 Author: Alessandro Massasso
 Author URI: http://www.eventualo.net
 */
